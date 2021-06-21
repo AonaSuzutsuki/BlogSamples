b47d95c90fbf78f2546e4e4f902b7a2333a46896
AonaSuzutsuki <rvvfake.org@gmail.com>
2021-06-21 10:20:03 +0900

    added files.

---
+++ SortableListView/SortableListView/ViewModels/ListViewValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortableListView.ViewModels
+{
+    public class ListViewValue
+    {
+        public int Id { get; set; }
+
+        public string Value1 { get; set; }
+
+        public string Value2 { get; set; }
+    }
+}
