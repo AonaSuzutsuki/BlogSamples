dd85cccc0c28d827ced4aed70fb72ca30a0004d9
AonaSuzutsuki <rvvfake.org@gmail.com>
2022-11-29 15:48:52 +0900

    refactored.

---
+++ SortableComparerListView/SortableComparerListView/ViewModels/FileListInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SortableComparerListView.ViewModels;
+
+public class FileListInfo
+{
+    public string Name { get; set; } = string.Empty;
+    public long FileSize { get; set; } = 0L;
+
+    public string FileSizeText
+    {
+        get
+        {
+            if (FileSize < 1024)
+                return $"{FileSize} Bytes";
+            else if (FileSize < 1024 * 1024)
+                return $"{Math.Floor((double)FileSize / 1024)} KB";
+            else if (FileSize < 1024 * 1024 * 1024)
+                return $"{Math.Floor((double)FileSize / (1024 * 1024))} MB";
+            else
+                return $"{Math.Floor((double)FileSize / (1024 * 1024 * 1024))} GB";
+        }
+    }
+}
--- SortableComparerListView/SortableComparerListView/ViewModels/MainWindowViewModel.cs
+++ SortableComparerListView/SortableComparerListView/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
@@ -7,27 +6,6 @@ using System.Threading.Tasks;
 
 namespace SortableComparerListView.ViewModels
 {
-    public class FileListInfo
-    {
-        public string Name { get; set; } = string.Empty;
-        public long FileSize { get; set; } = 0L;
-
-        public string FileSizeText
-        {
-            get
-            {
-                if (FileSize < 1024)
-                    return $"{FileSize} Bytes";
-                else if (FileSize < 1024 * 1024)
-                    return $"{Math.Floor((double)FileSize / 1024)} KB";
-                else if (FileSize < 1024 * 1024 * 1024)
-                    return $"{Math.Floor((double)FileSize / (1024 * 1024))} MB";
-                else
-                    return $"{Math.Floor((double)FileSize / (1024 * 1024 * 1024))} GB";
-            }
-        }
-    }
-
     public class MainWindowViewModel
     {
         public ICollection<FileListInfo> FileList { get; set; }
